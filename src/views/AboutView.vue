2e4513ef357729f83c984a85df1105b868fccbac
Mriganka Bora <mriganka@poshmark.com>
2023-08-03 13:07:34 +0530

    added event cards

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,13 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h2>A site for events around the world</h2>
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+.about {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,31 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue';
+import EventCard from '@/components/EventCard.vue';
+const eventData = ref ([
+  {
+    id: 1,
+    title: 'concert-1',
+    time: '9:30 pm',
+    date: '23/09/2023'
+  },
+   {
+    id: 2,
+    title: 'concert-2',
+    time: '5:30 pm',
+    date: '27/09/2023'
+  }
+])
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="events">
+    <EventCard v-for="event in eventData" :key='event.id' :event=event />
+  </div>
 </template>
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
